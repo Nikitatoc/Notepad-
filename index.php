984bd9a667aab8ac7f044b689fa21ade35febd0e
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2023-01-06 17:00:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require "head.html" ?>
+<form id="Login" method="post" action="api.php">
+    <input type="email" name="email" class="LoginForm" placeholder="Електронна пошта">
+    <br>
+    <br>
+    <input type="password" name="password" class="LoginForm" placeholder="Пароль">
+    <br>
+    <br>
+    <br>
+    <button type="submit" name="LoginButton" id="LoginButton">Вхід</button>
+</form>
+
+</body>
+</html>

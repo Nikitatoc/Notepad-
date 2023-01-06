71cbe2a5943e3a9e0c12ec5304970778a0c1adfe
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2023-01-06 17:05:15 +0200

    Create AllTask.php

---
+++ AllTask.php
@@ -0,0 +1,36 @@
+<?php require "head.html" ?>
+
+<div style="margin-top: 10%">
+    <a href="AllTask.php" class="TypeTasks" id="all">Усі</a>
+    <a href="WorkTask.php" class="TypeTasks">Робочі</a>
+    <a href="personal.php" class="TypeTasks">Особисті</a>
+</div>
+
+<?php
+$dns='mysql:host=localhost;dbname=List';
+$pdo= new PDO($dns, 'root','root');
+
+
+$sqlId = 'SELECT (id) as count FROM users WHERE user=?';
+$queryId = $pdo->prepare($sqlId);
+$queryId->execute([$_COOKIE['email']]);
+$ID = $queryId->fetch();
+$id=$ID[0];
+
+
+echo '<ul style="margin-top: 20%">';
+
+$query=$pdo ->query("SELECT * FROM `labor` WHERE id='$id'");
+while ($row=$query->fetch(PDO::FETCH_OBJ)){
+    echo '<li>'.$row->task. '<input type="checkbox" id="done">' .'<br>'.$row->time.  '<br>' .$row->urgently.'</li>';
+}
+$query=$pdo ->query("SELECT * FROM `personal` WHERE id='$id'");
+while ($row=$query->fetch(PDO::FETCH_OBJ)){
+    echo '<li>'.$row->task. '<input type="checkbox" id="done">' .'<br>'.$row->time. '<br>' .$row->urgently. '</li>';
+}
+echo '</ul>';
+?>
+
+<form action="CreateTask.php">
+    <button id= "CreateButton">+</button>
+</form>
